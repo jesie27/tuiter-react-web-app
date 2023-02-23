04cad589b885482a1e0e76caa1f024e65424cdb7
Jesie F <jesiefu11@gmail.com>
2023-02-22 21:57:44 -0500

    a6 React.js sp23

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -4,12 +4,14 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput
     from "./conditional-output";
+import TodoList from "./todo/todo-list";
 function Assignment6() {
     return (
         <div>
 
             <h1>Assignment 6</h1>
             <ConditionalOutput/>
+            <TodoList/>
             <Styles/>
             <Classes/>
         </div>
--- src/labs/a6/todo/todo-list.js
+++ src/labs/a6/todo/todo-list.js
@@ -3,7 +3,7 @@ import todos from "./todos.json";
 const TodoList = () => {
     return(
         <>
-            <h3>TodoList</h3>
+            <h3>Todo List</h3>
             <ul>
                 {
                     todos.map(todo => {
