7dddb54093221b3d9464d13396b727557d0cfe66
Jesie F <jesiefu11@gmail.com>
2023-03-21 09:59:04 -0400

    a7 Redux sp23

--- src/tuiter/tuits/TuitItem.js
+++ src/tuiter/tuits/TuitItem.js
@@ -41,17 +41,17 @@ const TuitItem = ({
                     <TuitStats/>
 
 
-                    <p className="mb-0 wd-fg-color-gray ms-5">
-                        <i className="bi bi-chat-left-dots pe-2"></i>
-                        {post.comments}
-                        <i className="bi bi-repeat ps-3 pe-2"></i>
-                        {post.retuits}
+                    {/*<p className="mb-0 wd-fg-color-gray ms-5">*/}
+                    {/*    <i className="bi bi-chat-left-dots pe-2"></i>*/}
+                    {/*    {post.comments}*/}
+                    {/*    <i className="bi bi-repeat ps-3 pe-2"></i>*/}
+                    {/*    {post.retuits}*/}
 
-                        <i className="bi bi-heart-fill ps-3 pe-2"></i>
-                        {post.likes}
+                    {/*    <i className="bi bi-heart-fill ps-3 pe-2"></i>*/}
+                    {/*    {post.likes}*/}
 
-                        <i className="bi bi-upload ps-3"></i>
-                    </p>
+                    {/*    <i className="bi bi-upload ps-3"></i>*/}
+                    {/*</p>*/}
 
 
 
--- src/tuiter/tuits/TuitStats.js
+++ src/tuiter/tuits/TuitStats.js
@@ -21,10 +21,20 @@ const TuitStats = () => {
             {/*    <i className="bi bi-upload ps-3"></i>*/}
             {/*</p>*/}
 
-            <button className=""
-                    onClick={likeClickHandler}>
-                Like
+            <p className="mb-0 wd-fg-color-gray ms-5">
+                <i className="bi bi-chat-left-dots pe-2"></i>
+                {tuits.comments}
+                <i className="bi bi-repeat ps-3 pe-2"></i>
+                {tuits.retuits}
+                <button className=""
+                        onClick={likeClickHandler}>
+                <i className="bi bi-heart-fill ps-3 pe-2"></i>
+                {tuits.likes}
             </button>
+
+                <i className="bi bi-upload ps-3"></i>
+            </p>
+
         </div>
 
     );
