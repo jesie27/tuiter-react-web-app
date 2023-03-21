a76afed4a776e37ad13ae9f34ecd7283a2bcc054
Jesie F <jesiefu11@gmail.com>
2023-03-20 21:54:49 -0400

    a7 Redux sp23

--- src/tuiter/tuits/homeindex2.js
+++ src/tuiter/tuits/homeindex2.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeSummaryList2 from "./TuitsList";
-
+import WhatsHappening from "../home/whats-happening";
 function HomeComponent2(post) {
     return (
 
         <div>
+            <h4>Home</h4>
+            <WhatsHappening/>
             <HomeSummaryList2/>
         </div>
     );
