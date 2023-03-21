cc84a6360a8facaa8bd57f3366eb0285459a067b
Jesie F <jesiefu11@gmail.com>
2023-03-21 12:23:36 -0400

    a7 Redux sp23

--- src/tuiter/tuits/TuitStats.js
+++ src/tuiter/tuits/TuitStats.js
@@ -3,27 +3,15 @@ import tuits from './tuits.json';
 import TuitItem from "./TuitItem";
 import React, {useState} from "react";
 
-const TuitStats = () => {
+const TuitStats = (tuits,index) => {
     let [initialLikes, setLikes] = useState('');
     const likeClickHandler = () => {
     }
     return (
         <div>
-            {/*<p className="mb-0 wd-fg-color-gray ms-5">*/}
-            {/*    <i className="bi bi-chat-left-dots pe-2"></i>*/}
-            {/*    {comments}*/}
-            {/*    <i className="bi bi-repeat ps-3 pe-2"></i>*/}
-            {/*    {retuits}*/}
-
-            {/*    <i className="bi bi-heart-fill ps-3 pe-2"></i>*/}
-            {/*    {likes}*/}
-
-            {/*    <i className="bi bi-upload ps-3"></i>*/}
-            {/*</p>*/}
-
             <p className="mb-0 wd-fg-color-gray ms-5">
                 <i className="bi bi-chat-left-dots pe-2"></i>
-                {tuits.comments}
+                {tuits.replies}
                 <i className="bi bi-repeat ps-3 pe-2"></i>
                 {tuits.retuits}
                 <button className=""
