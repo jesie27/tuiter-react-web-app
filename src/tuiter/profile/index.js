8df66f3903b3d3075941bd9058f7e489d9dc56be
Jesie F <jesiefu11@gmail.com>
2023-03-26 21:46:23 -0400

    a7 Redux sp23

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-function ProfileComponent(profile) {
+const ProfileComponent=(
+    {   profile = {
+        "firstName": "Jesie",
+        "lastName": "F",
+        "handle": "@jesie",
+        "profilePicture": "corgi.png",
+        "bannerPicture": "corgi-bench.png",
+        "bio": "Computer Scientist",
+        "website": "youtube.com/webdevtv",
+        "location": "Boston, MA",
+        "dateOfBirth": "7/11/2023",
+        "dateJoined": "3/2023",
+        "followingCount": 340,
+        "followersCount": 223
+        }
+    }
+) =>
+{
+
     return (
         <div>
-            <h4>Profle</h4>
+            <h4>Profile</h4>
+            {profile.bio}
 
         </div>
     );
