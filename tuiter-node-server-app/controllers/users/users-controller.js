8a008c907ea6e11f2b8c88544eabe105e51fd804
Jesie F <jesiefu11@gmail.com>
2023-04-01 23:16:08 -0400

    a8 REST fa22

--- tuiter-node-server-app/controllers/users/users-controller.js
+++ tuiter-node-server-app/controllers/users/users-controller.js
@@ -6,6 +6,7 @@ const UserController = (app) => {
     app.get('/api/users/:uid', findUserById);
     app.post('/api/users', createUser);
     app.delete('/api/users/:uid', deleteUser);
+    app.put('/api/users/:uid', updateUser);
 }
 const createUser = (req, res) => {
     const newUser = req.body;
@@ -17,6 +18,17 @@ const deleteUser = (req, res) => {
     const userId = req.params['uid'];
     users = users.filter(usr =>
         usr._id !== userId);
+    res.sendStatus(200);
+}
+
+const updateUser = (req, res) => {
+    const userId = req.params['uid'];
+    const updates = req.body;
+    users = users.map((usr) =>
+        usr._id === userId ?
+            {...usr, ...updates} :
+            usr
+    );
     res.sendStatus(200);
 }
 
